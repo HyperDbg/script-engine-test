90fae71e5fefb64b844b58b4272e7f4f88b33d03
SinaKarvandi <ms.karvandi@yahoo.com>
2021-12-05 15:21:42 +0330

    add c# script evaluator

---
+++ EvalScripts/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+namespace EvalScripts
+{
+    class Program
+    {
+        public static bool EvalScriptRun(string Script)
+        {
+            try
+            {
+                CSharpScript.EvaluateAsync(Script);
+
+                //
+                // Script run without error
+                //
+                return true;
+            }
+            catch (Exception)
+            {
+
+                //
+                // Script has error
+                //
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            bool result = EvalScriptRun(@"
+            
+            for (int i = 0; i < 100; i++) 
+            {
+                System.Console.WriteLine(i);
+            }
+
+            ");
+
+            if (!result)
+            {
+                Console.WriteLine("Script has error");
+            }
+            else
+            {
+                Console.WriteLine("Script run without error");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
